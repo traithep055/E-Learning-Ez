34a771c19d700cd938233b5de0b0b061e8d9c3de
Traithep055 <ais4255bk@gmail.com>
2024-07-05 12:38:48 +0700

    feat: ตกแต่งหน้า Sign up

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,115 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!doctype html>
+<html lang="en">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ez Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</head>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<body style="background-image: url('{{ asset('images/back.jpg') }}');">
+    <div class="container">
+        <div class="forms">
+            <div class="form login">
+                <span class="title">สร้างบัญชี Ez ของคุณ</span>
+                <div class="sub-title">
+                    <span เพื่อพบกับคอร์สเรียนหลากหลายสาขา>เพื่อพบกับคอร์สเรียนหลากหลายสาขา</span>
+                </div>
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
+                    <div class="input-field">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                        <input id="name" type="text" name="name" value="{{old('name')}}" placeholder="Enter your Name" required>
+                        <i class="uil uil-user"></i>
+                    </div>
+                    @if($errors->has('name'))
+                            <span class="error" style="color: red">{{ $errors->first('name') }}</span>
+                    @endif
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                    <div class="input-field">
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                        <input id="email" type="email" name="email" value="{{old('email')}}" placeholder="Enter your email" required>
+                        <i class="uil uil-envelope"></i>
+                    </div>
+                    @if($errors->has('email'))
+                            <span class="error" style="color: red">{{ $errors->first('email') }}</span>
+                    @endif
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                    <div class="input-field">
+                        <input type="password" id="password" name="password" class="password" placeholder="Enter your password"
+                            required autocomplete="new-password">
+                        <i class="uil uil-lock icon"></i>
+                        <i class="uil uil-eye-slash showHidePw"></i>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                    </div>
+                    @if($errors->has('password'))
+                            <span class="error" style="color: red">{{ $errors->first('password') }}</span>
+                    @endif
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+                    <div class="input-field">
+                        <input type="password" id="password_confirmation" name="password_confirmation" class="password" placeholder="Confirm your password"
+                            required autocomplete="new-password">
+                        <i class="uil uil-lock icon"></i>
+                        <i class="uil uil-eye-slash showHidePw"></i>
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+
+                    </div>
+                    @if($errors->has('password_confirmation'))
+                            <span class="error" style="color: red">{{ $errors->first('password_confirmation') }}</span>
+                    @endif
+
+                    <div class="input-field">
+                        <style>
+                            input[type="submit"] {
+                                background-color: #7dba7c;
+                                /* สีม่วง */
+                                color: #ffffff;
+                                /* สีข้อความ */
+                                padding: 10px 20px;
+                                /* กำหนดขนาดพื้นที่ภายในปุ่ม */
+                                border: none;
+                                /* ลบเส้นขอบ */
+                                border-radius: 5px;
+                                /* ทำมุมขอบเรียบ */
+                            }
+                            /* เม้าส์ไปชี้ปุ่ม Submit */
+                            input[type="submit"]:hover {
+                                background-color: #E8DFCA;
+                                color: black;
+                                /* สีม่วงอ่อน */
+                            }
+                        </style>
+                        <input type="submit" value="Sign up">
+                    </div>
+                </form>
+            </div>
+
+            <script>
+                const container = document.querySelector(".container"),
+                    pwShowHide = document.querySelectorAll(".showHidePw"),
+                    pwFields = document.querySelectorAll(".password");
+                pwShowHide.forEach(eyeIcon => {
+                    eyeIcon.addEventListener("click", () => {
+                        pwFields.forEach(pwField => {
+                            if (pwField.type === "password") {
+                                pwField.type = "text";
+                                pwShowHide.forEach(icon => {
+                                    icon.classList.replace("uil-eye-slash", "uil-eye");
+                                })
+                            } else {
+                                pwField.type = "password";
+                                pwShowHide.forEach(icon => {
+                                    icon.classList.replace("uil-eye", "uil-eye-slash");
+                                })
+                            }
+                        });
+                    });
+                });
+            </script>
+            <script src="script.js"></script>
+
+</body>
+</html>
