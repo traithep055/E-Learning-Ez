116976f544de32fdf55278968ff6b835a7845ff7
koi <sunisaixk@gmail.com>
2024-08-11 18:18:18 +0700

    feat : สร้างรูปแบบตัวแพคเกจ

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -20,22 +20,22 @@
     {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> --}}
 
     <!-- Insert css -->
-    <link rel="stylesheet" href="{{asset('frontend/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/select2.min.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/slick.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/jquery.nice-number.min.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/jquery.calendar.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/add_row_custon.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/mobile_menu.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/jquery.exzoom.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/multiple-image-video.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/ranger_style.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/jquery.classycountdown.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/venobox.min.css')}}">
-
-    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery.nice-number.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery.calendar.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/add_row_custon.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/mobile_menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery.exzoom.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/multiple-image-video.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/ranger_style.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/jquery.classycountdown.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/venobox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/packet.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
 
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
@@ -53,13 +53,13 @@
 <body>
 
     {{-- header start --}}
-        @include('frontend.layouts.header')
+    @include('frontend.layouts.header')
     {{-- header end --}}
 
     <!--///////////////////// -->
     {{-- start main content --}}
     {{-- <article class="article"> --}}
-        @yield('content')
+    @yield('content')
     {{-- </article> --}}
     {{-- end main content --}}
 
@@ -74,57 +74,57 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
 
     {{-- ////start footer//// --}}
-        @include('frontend.layouts.footer')
+    @include('frontend.layouts.footer')
     {{-- ////end footer//// --}}
 
     <!--jquery library js-->
-    <script src="{{asset('frontend/js/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/jquery-3.6.0.min.js') }}"></script>
     <!--bootstrap js-->
-    <script src="{{asset('frontend/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
     <!--font-awesome js-->
-    <script src="{{asset('frontend/js/Font-Awesome.js')}}"></script>
+    <script src="{{ asset('frontend/js/Font-Awesome.js') }}"></script>
     <!--select2 js-->
-    <script src="{{asset('frontend/js/select2.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/select2.min.js') }}"></script>
     <!--slick slider js-->
-    <script src="{{asset('frontend/js/slick.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/slick.min.js') }}"></script>
     <!--simplyCountdown js-->
-    <script src="{{asset('frontend/js/simplyCountdown.js')}}"></script>
+    <script src="{{ asset('frontend/js/simplyCountdown.js') }}"></script>
     <!--product zoomer js-->
-    <script src="{{asset('frontend/js/jquery.exzoom.js')}}"></script>
+    <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
     <!--nice-number js-->
-    <script src="{{asset('frontend/js/jquery.nice-number.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/jquery.nice-number.min.js') }}"></script>
     <!--counter js-->
-    <script src="{{asset('frontend/js/jquery.waypoints.min.js')}}"></script>
-    <script src="{{asset('frontend/js/jquery.countup.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery.countup.min.js') }}"></script>
     <!--add row js-->
-    <script src="{{asset('frontend/js/add_row_custon.js')}}"></script>
+    <script src="{{ asset('frontend/js/add_row_custon.js') }}"></script>
     <!--multiple-image-video js-->
-    <script src="{{asset('frontend/js/multiple-image-video.js')}}"></script>
+    <script src="{{ asset('frontend/js/multiple-image-video.js') }}"></script>
     <!--sticky sidebar js-->
-    <script src="{{asset('frontend/js/sticky_sidebar.js')}}"></script>
+    <script src="{{ asset('frontend/js/sticky_sidebar.js') }}"></script>
     <!--price ranger js-->
-    <script src="{{asset('frontend/js/ranger_jquery-ui.min.js')}}"></script>
-    <script src="{{asset('frontend/js/ranger_slider.js')}}"></script>
+    <script src="{{ asset('frontend/js/ranger_jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/ranger_slider.js') }}"></script>
     <!--isotope js-->
-    <script src="{{asset('frontend/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
     <!--venobox js-->
-    <script src="{{asset('frontend/js/venobox.min.js')}}"></script>
+    <script src="{{ asset('frontend/js/venobox.min.js') }}"></script>
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <script>
         @if ($errors->any())
             @foreach ($errors->all() as $error)
-                toastr.error("{{$error}}")
+                toastr.error("{{ $error }}")
             @endforeach
         @endif
     </script>
 
     <!--classycountdown js-->
-    <script src="{{asset('frontend/js/jquery.classycountdown.js')}}"></script>
+    <script src="{{ asset('frontend/js/jquery.classycountdown.js') }}"></script>
 
     <!--main/custom js-->
-    <script src="{{asset('frontend/js/main.js')}}"></script>
+    <script src="{{ asset('frontend/js/main.js') }}"></script>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
@@ -132,4 +132,5 @@
 
     @stack('scripts')
 </body>
+
 </html>
--- resources/views/frontend/pages/package.blade.php
+++ resources/views/frontend/pages/package.blade.php
@@ -1,66 +1,69 @@
-@extends('frontend.layouts.master')
+    <link rel="stylesheet" href="{{ asset('frontend/css/packet.css') }}">
 
-@section('content')
+    @extends('frontend.layouts.master')
 
-<div class="container mt-8" style="margin-left: 5%">
-    <div class="row">
-        <div class="col mt-3">
-            <div class="content d-flex flex-column " style="padding: 2%;">
-                <div class="row mt-3" style="margin-top: 30px; padding: 1%;">
-
-                    {{-- Start ตัวกรอง --}}
-                        <div class="menu" style="width: 25%; margin-left: 40px">
-                            <div class="price mt-3 mb-3" style="display: flex; align-items: center;">
-                                <h4>แพ็คเกจทั้งหมด</h4>
-                            </div>
-                            <div class="divider" style="width: 90%;"></div>
-                        </div>
-                    {{-- End ตัวกรอง --}}
-                    <div class="col-md-8" style="margin-left: 55px">
-                        <div class="row">
-                            @forelse ($packages as $package)
-                                <div class="col-md-4 mb-5 d-flex align-items-stretch">
-                                    <div class="card card-hover">
-                                        {{-- <a href="{{ route('course-detail', ['id' => $course->id]) }}">
+    @section('content')
+        <div class="container mt-8" style="margin-left: 5%, bachground-color: #55f5f5">
+            {{-- Start ตัวกรอง --}}
+            <div class="menu" style="width: 25%; margin-left: 40px">
+                <div class="price mt-3 mb-3" style="display: flex; align-items: center;">
+                    <h4>แพ็คเกจทั้งหมด</h4>
+                </div>
+                <di-v class="divider" style="width: 90%;"></di-v>
+            </div>
+            {{-- End ตัวกรอง --}}
+            <div class="col-md-8" style="margin-left: 55px">
+                <div class="row">
+                    @forelse ($packages as $package)
+                        <div class="col-md-4 mb-5 d-flex align-items-stretch">
+                            <div class="card-package">
+                                {{-- <a href="{{ route('course-detail', ['id' => $course->id]) }}">
                                             <img src="{{ $course->image }}" class="card-img-top" alt="...">
                                         </a> --}}
-                                        <div class="card-body">
-                                            <h5 class="card-title">
-                                                <a href="">{{ $package->name }}</a>
-                                            </h5>
-                                            <span><a href="#">{{ $package->description }}</a></span>
-                                            <p class="card-text">
-                                                <strong>ระยะเวลา:</strong>
-                                                @switch($package->duration)
-                                                    @case('1_year')
-                                                        1 ปี
-                                                        @break
-                                                    @case('2_years')
-                                                        2 ปี
-                                                        @break
-                                                    @case('3_years')
-                                                        3 ปี
-                                                        @break
-                                                    @default
-                                                        ไม่ระบุ
-                                                @endswitch
-                                            </p>
-                                            <p class="card-text" style="color: #FF6347; font-size: 18px;"><strong>ราคา: {{ $package->price }} บาท</strong></p>
-                                            <a href="{{ route('user.package_purchase', ['package' => $package->id]) }}" class="btn btn-primary">ชื้อแพ็คเกจ</a>
-                                        </div>
-                                    </div>
+                                <div class="card-body-package">
+                                    <h5 class="title-package">
+                                        <p href="">{{ $package->name }}</p>
+                                    </h5>
+                                    <h5 class="percent">
+                                        <p> 50 %</p>
+                                        <span>/ คอร์ส </span>
+                                    </h5>
+                                    <span>
+                                        <p>{{ $package->description }}</p>
+                                    </span>
+                                    <div class="line"></div>
+                                    <p class="card-text-package">
+                                        <strong>ระยะเวลา:</strong>
+                                        @switch($package->duration)
+                                            @case('1_year')
+                                                1 ปี
+                                            @break
+
+                                            @case('2_years')
+                                                2 ปี
+                                            @break
+
+                                            @case('3_years')
+                                                3 ปี
+                                            @break
+
+                                            @default
+                                                ไม่ระบุ
+                                        @endswitch
+                                    </p>
+                                    <p class="card-text-package" style="color: #ffffff; font-size: 18px;"><strong>ราคา:
+                                            {{ $package->price }} บาท</strong></p>
+                                    <a href="{{ route('user.package_purchase', ['package' => $package->id]) }}"
+                                        class="btn-package">ชื้อแพ็คเกจ</a>
                                 </div>
-                                @empty
-                                    <div class="col-12">
-                                        <p>ไม่มีแพ็คเกจที่พร้อมให้บริการในขณะนี้</p>
-                                    </div>
-                                @endforelse
+                            </div>
                         </div>
+                        @empty
+                            <div class="col-12">
+                                <p>ไม่มีแพ็คเกจที่พร้อมให้บริการในขณะนี้</p>
+                            </div>
+                        @endforelse
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-
-@endsection
+        @endsection
